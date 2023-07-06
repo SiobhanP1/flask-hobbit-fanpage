81428c267e98f7306ae6a62550b94643190467c9
SiobhanP1 <siobhanphillips11@gmail.com>
2023-07-06 16:09:05 +0000

    Add contact html page and contact view in run.py

--- run.py
+++ run.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 # When browse to root directory (/), triggers index() function
 @app.route("/")
 def index():
-    return render_template("templates/index.index.html")
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 
 # Must set debug to False when finished developing.
 if __name__ == "__main__":
